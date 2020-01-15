c616545ad1fb196be38e91cc90e6591339dc031c
GIOVANNI RUSSO BOSCOLI <boscoli.giovanni@outlook.com>
2020-01-15 21:42:54 +0000

    Removing unwanted files

--- TimesheetScheduler.BLL/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace TimesheetScheduler.BLL
-{
-    public class Class1
-    {
-    }
-}
--- TimesheetScheduler.DAL/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace TimesheetScheduler.DAL
-{
-    public class Class1
-    {
-    }
-}
